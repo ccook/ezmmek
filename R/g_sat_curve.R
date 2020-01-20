6d87185bd9fd1e426d49c0f2b1bfbdcd559107e2
chris <ccook62@vols.utk.edu>
2020-01-20 16:24:33 -0500

    functionality of German method complete

--- R/g_sat_curve.R
+++ R/g_sat_curve.R
@@ -1,4 +1,4 @@
-g_sat_curve <- function(d_std_g, d_sat_g, x.label = NULL, y.label = NULL) {
+g_sat_curve <- function(d_std_g, d_sat_g, x.label = NULL, y.label = NULL, km = NULL, vmax = NULL) {
 
   ### Stop function if columns lack these specific names
   assertable::assert_colnames(data = d_std_g,
@@ -20,52 +20,57 @@ g_sat_curve <- function(d_std_g, d_sat_g, x.label = NULL, y.label = NULL) {
                               quiet = TRUE)
 
   ### Add mean spec values of standard in buffer and standard in homogenate
-  d_std_g_mean <- d_std_g %>%
+  d_std_g_2 <- d_std_g %>%
     dplyr::group_by(std.conc) %>%
     dplyr::mutate(spec.m.homo = mean(spec.homo)) %>%
     dplyr::mutate(spec.m.buffer = mean(spec.buffer))
 
   ### Calculate slope of mean spec value of standard in homogenate
-  lm_fit_homo <- lm(spec.m.homo ~ std.conc, data = d_std_g_mean)
+  lm_fit_homo <- lm(spec.m.homo ~ std.conc, data = d_std_g_2)
   lm.slope.homo <- coef(lm_fit_homo)[2]
 
   ### Calculate slope of mean spec value of standard in buffer
-  lm_fit_buffer <- lm(spec.m.buffer ~ std.conc, data = d_std_g_mean)
+  lm_fit_buffer <- lm(spec.m.buffer ~ std.conc, data = d_std_g_2)
   lm.slope.buffer <- coef(lm_fit_buffer)[2]
 
   ### Add quench coef. and emission coef. to raw saturation data
-  d_sat_g_coef <- d_sat_g %>%
-    dplyr::group_by(sub.conc) %>%
+  d_sat_g_2 <- d_sat_g %>%
+    dplyr::group_by(sub.conc, time) %>%
     dplyr::mutate(quench.coef = lm.slope.homo / lm.slope.buffer) %>%
     dplyr::mutate(emission.coef = (lm.slope.homo / assay.vol))
 
-  ### Create new dataframe that calculates mean homogenate control spec values over time
-  d_sat_baseline <- d_sat_g_coef %>%
+  ### Create new dataframe that contains average spec values of homogenate controls over time
+  d_sat_baseline <- d_sat_g_2 %>%
     filter(sub.conc == 0) %>%
     group_by(time) %>%
     summarize(spec.baseline.m = mean(spec),
               spec.baseline.sd = sd(spec))
 
-  d_sat_g_merge <- merge(d_sat_g_coef, d_sat_baseline, by = ("time"))
+  ### Create new dataframe that merges d_sat_g_2 with homogenate control values from d_sat_baseline
+  d_sat_g_3 <- merge(d_sat_g_2, d_sat_baseline, by = ("time")) %>%
+    dplyr::filter(time > 0) #Filter out Time 0, because cannot divide by 0 later
 
-  d_sat_g_merge$spec.homogenate.corrected <- d_sat_g_merge$spec - d_sat_g_merge$spec.baseline.m
+  ### Correct spec values by homogenate baseline control
+  d_sat_g_3$spec.homogenate.corrected <- d_sat_g_3$spec - d_sat_g_3$spec.baseline.m
 
-  d_sat_g_merge$time[d_sat_g_merge$time == 0]<-0.001
-
-  d_sat_g_act <- d_sat_g_merge %>%
+  ### Create new dataframe that calculate net fluorescence and activity
+  d_sat_g_4 <- d_sat_g_3 %>%
     dplyr::mutate(net.fluor = (spec.homogenate.corrected / quench.coef) - sub.control) %>%
     dplyr::mutate(activity = (net.fluor * buffer.vol) / (emission.coef * homo.vol * time * soil.mass))
 
-  d_sat_g_act_avg <- d_sat_g_act %>%
+  ### Create new dataframe that contains mean and standard deviations of activity by substrate concentration
+  d_sat_g_5 <- d_sat_g_4 %>%
     dplyr::group_by(sub.conc) %>%
-    dplyr::mutate(activity.m = mean(activity), activity.sd = sd(activity))
+    dplyr::mutate(activity.m = mean(activity), activity.sd = sd(activity)) %>%
+    dplyr::filter(sub.conc > 0)
 
 
   ### Assign values for x-axis and y-axis labels on plot
   x.label <- x.label
   y.label <- y.label
 
-  g_sat_curve_plot <- ggplot2::ggplot(data = d_sat_g_act_avg,
+  ### Create plot with substrate conc. as x-axis, and average activity as y-axis
+  g_sat_curve_plot <- ggplot2::ggplot(data = d_sat_g_5,
                                       mapping = ggplot2::aes(x = sub.conc, y = activity.m)) +
     ggplot2::geom_point() +
     ggplot2::theme_bw() +
@@ -73,34 +78,52 @@ g_sat_curve <- function(d_std_g, d_sat_g, x.label = NULL, y.label = NULL) {
     ggplot2::ylab(y.label) +
     ggplot2::theme(axis.text = ggplot2::element_text(),
                    axis.title = ggplot2::element_text()) +
-    ggplot2::geom_errorbar(ggplot2::aes(ymin = activity.m - activity.sd, ymax = activity.m + activity.sd))
+    ggplot2::geom_errorbar(ggplot2::aes(ymin = activity.m - activity.sd, ymax = activity.m + activity.sd)) +
     ggplot2::scale_y_continuous(labels = scales::scientific)
 
-  max.activity <- max(d_sat_g_act_avg$activity.m)
-  half.conc <- median(d_sat_g_act_avg$sub.conc)
+  ### Calculate saturation curve fit
+  max.activity <- max(d_sat_g_5$activity.m)
+  half.conc <- median(d_sat_g_5$sub.conc)
 
-  mm_form <- formula(activity.m ~ (Vmax * sub.conc)/(Km + sub.conc))
-  mm_fit <- nls2::nls2(formula = mm_form, data = d_sat_g_act_avg,
-                       start = list(Vmax = max.activity, Km = half.conc))
+  if(is.null(km) | is.null(vmax)) {
 
+    mm_form <- formula(activity.m ~ (vmax * sub.conc)/(km + sub.conc))
+    mm_fit <- nls2::nls2(formula = mm_form, data = d_sat_g_5,
+                         start = list(vmax = max.activity, km = half.conc))
+  } else {
+
+    km <- km
+    vmax <- vmax
+
+    mm_form <- formula(activity.m ~ (vmax * sub.conc)/(km + sub.conc))
+    mm_fit <- nls2::nls2(formula = mm_form, data = d_sat_g_5,
+                         start = list(vmax = vmax, km = km))
+  }
+
+  ### ## Print km and vmax variables to console
   print(summary(mm_fit))
 
-  min.sub.conc <- min(d_sat_g_act_avg$sub.conc)
-  max.sub.conc <- max(d_sat_g_act_avg$sub.conc)
+  ### Create a 1-column data frame with a 'grid' of points to predict
+  min.sub.conc <- min(d_sat_g_5$sub.conc)
+  max.sub.conc <- max(d_sat_g_5$sub.conc)
   pred_grid <- data.frame(sub.conc = min.sub.conc:max.sub.conc)
 
+  ### Put the predicted values into a data frame, paired with the values at which they were predicted
   predictions <- predict(mm_fit, newdata = pred_grid)
 
   pred_df <- data.frame(sub.conc = pred_grid$sub.conc, activity.m = predictions)
 
+  ### Create and print plot of saturation curve with fit
   sat_fit <- g_sat_curve_plot +
     ggplot2::geom_line(data = pred_df, ggplot2::aes(x = sub.conc, y = activity.m))
 
   plot(sat_fit)
 
-  outlist <- list(sat_data = d_sat_g_act_avg,
-                  #fit_buffer = lm_fit_buffer,
-                  plot_object = g_sat_curve_plot)
+  ### Output list of predicted Vmax, predicted Km, d_sat_2 dataframe, mm_fit summary, and plot
+  outlist <- list(sat_data = d_sat_g_5,
+                  curve_data = pred_df,
+                  fit_object = mm_fit,
+                  plot_object = sat_fit)
 
 
 }
--- R/g_std_curve.R
+++ R/g_std_curve.R
@@ -1,3 +1,30 @@
+##' Create standard curves using German method
+##'
+##' @description Creates a standard curve of spectral data versus fluorophore/chromophore concentration.
+##' Linear models and standard curves are generated for the standard in the presence of buffer and homogenate.
+##' A third plot that overlays both standard curves is also generated.
+##'
+##' @param d_std_g  Must be a dataframe that contains 'std.conc', 'replicate', 'spec.homo', and 'spec.buffer'.
+##' @param x.label The user must input a character string in the second argument to label the x-axis.
+##' @param y.label The user must input a character string in the third argument to label the y-axis.
+##'
+##' @return List containing plots, fit models, and new dataframe.
+##'
+##' @details Plots spectral data vs standard concentration.
+##' The user can label the x-axis by inputting a second argument.
+##' The user can label the y-axis by inputting a third argument.
+##' If the user does not define these arguments, the function will default to create a plot without labels.
+##'
+##' @examples
+##' std_curve(d_std_g)
+##' std_curve(d_std_g, "x-axis", "y-axis")
+##'
+##' @author Christopher L. Cook and Andrew D. Steen
+##' @export
+
+########
+#Plot standard curve and print linear model stats
+########
 g_std_curve <- function(d_std_g, x.label = NULL, y.label = NULL) {
 
 
@@ -8,13 +35,13 @@ g_std_curve <- function(d_std_g, x.label = NULL, y.label = NULL) {
                               quiet = TRUE)
 
   ### Add mean spec values of standard in buffer and standard in homogenate to new dataframe
-  d_std_g_mean <- d_std_g %>%
+  d_std_g_2 <- d_std_g %>%
     dplyr::group_by(std.conc) %>%
     dplyr::mutate(spec.m.homo = mean(spec.homo)) %>%
     dplyr::mutate(spec.m.buffer = mean(spec.buffer))
 
   ### Calculate slope of mean spec value of standard in homogenate
-  lm_fit_homo <- lm(spec.m.homo ~ std.conc, data = d_std_g_mean)
+  lm_fit_homo <- lm(spec.m.homo ~ std.conc, data = d_std_g_2)
   print(summary(lm_fit_homo))
 
   ### Calculate slope of mean spec value of standard in buffer
@@ -24,7 +51,34 @@ g_std_curve <- function(d_std_g, x.label = NULL, y.label = NULL) {
   x.label <- x.label
   x.label <- y.label
 
-  g_std_curve_plot <- ggplot2::ggplot() +
+  ### Generate plot of standard curve in homogenate
+  g_std_curve_homo <- ggplot2::ggplot(data = d_std_g,
+                                           mapping = aes(x = std.conc, y = spec.homo)) +
+    ggplot2::geom_point() +
+    ggplot2::theme_bw() +
+    ggplot2::xlab(x.label) +
+    ggplot2::ylab(y.label) +
+    ggplot2::scale_y_continuous(labels = scales::scientific) +
+    ggplot2::theme(axis.text = ggplot2::element_text(), axis.title = ggplot2::element_text()) +
+    ggplot2::geom_smooth(method = "lm", color = "blue")
+
+  print(g_std_curve_homo)
+
+  ### Generate plot of standard curve in buffer
+  g_std_curve_buffer <- ggplot2::ggplot(data = d_std_g,
+                                           mapping = aes(x = std.conc, y = spec.buffer)) +
+    ggplot2::geom_point() +
+    ggplot2::theme_bw() +
+    ggplot2::xlab(x.label) +
+    ggplot2::ylab(y.label) +
+    ggplot2::scale_y_continuous(labels = scales::scientific) +
+    ggplot2::theme(axis.text = ggplot2::element_text(), axis.title = ggplot2::element_text()) +
+    ggplot2::geom_smooth(method = "lm", color = "red")
+
+  print(g_std_curve_buffer)
+
+  ### Generate plot overlaying both standard curves
+  g_std_curve_overlay <- ggplot2::ggplot() +
     ggplot2::geom_point(data = d_std_g_mean, ggplot2::aes(x = std.conc, y = spec.buffer, color = "blue")) +
     ggplot2::geom_point(data = d_std_g_mean, ggplot2::aes(x = std.conc, y = spec.homo, color = "red")) +
     ggplot2::theme_bw() +
@@ -36,11 +90,14 @@ g_std_curve <- function(d_std_g, x.label = NULL, y.label = NULL) {
     ggplot2::geom_smooth(data = d_std_g_mean, ggplot2::aes(x = std.conc, y = spec.homo), method = "lm", color = "red") +
     ggplot2::scale_color_discrete(name = "Solution Type", labels = c("Buffer", "Homogenate"))
 
-  print(g_std_curve_plot)
+  print(g_std_curve_overlay)
 
-  outlist <- list(std_data = d_std_g_mean,
-                  fit_buffer = lm_fit_buffer,
-                  fit_homo = lm_fit_homo,
-                  plot_object = g_std_curve_plot)
+  ### Output list containing new dataframe, linear models, and plots
+  outlist <- list(std_data = d_std_g_2,
+                  fit_buffer = summary(lm_fit_buffer),
+                  fit_homogenate = summary(lm_fit_homo),
+                  plot_object_overlay = g_std_curve_overlay,
+                  plot_object_homogenate = g_std_curve_homo,
+                  plot_object_buffer = g_std_curve_buffer)
 }
 
